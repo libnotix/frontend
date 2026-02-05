6aa0cfedc05b976d8af0901aff75f53c98158a8e
MrMancs <marcell.szatmari2007@gmail.com>
2026-02-05 09:00:25 +0100

    Create registration page layout

--- app/createaccount/page.tsx
+++ app/createaccount/page.tsx
@@ -1,5 +1,5 @@
-import { memo } from 'react';
-import { Button } from "@/components/ui/button"
+import { memo } from "react";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardAction,
@@ -8,56 +8,64 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 const CreateAccount = () => {
   return (
-    <div className='container flex items-center justify-center mx-auto h-full rounded-[1rem]'>
+    <div className="container flex items-center justify-center mx-auto h-full rounded-[1rem]">
       <Card className="w-full max-w-sm rounded-[1rem]">
         <CardHeader>
-          <CardTitle>Bejelentkezés</CardTitle>
-          <CardDescription>
-        aaaa
-    </CardDescription>
-          <CardAction>
-            <Button variant="link">Fiók létrehozása</Button>
-          </CardAction>
+          <CardTitle>Regisztráció</CardTitle>
+          <CardDescription>Hozz létre egy új fiókot!</CardDescription>
         </CardHeader>
         <CardContent>
           <form>
             <div className="flex flex-col gap-6">
               <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
+                <Label htmlFor="lastName">Vezetéknév</Label>
                 <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
+                  id="lastName"
+                  type="text"
+                  placeholder="Minta"
                   required
                 />
               </div>
               <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
+                <Label htmlFor="firstName">Keresztnév</Label>
+                <Input
+                  id="firstName"
+                  type="text"
+                  placeholder="János"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="minta@gmail.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="username">Felhasználónév</Label>
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="MintaJanos"
+                  required
+                />
               </div>
             </div>
           </form>
         </CardContent>
         <CardFooter className="flex-col gap-2">
           <Button type="submit" className="w-full">
-            Login
-          </Button>
-          <Button variant="outline" className="w-full">
-            Login with Google
+            <Link href="/login">Regisztráció</Link>
           </Button>
         </CardFooter>
       </Card>
